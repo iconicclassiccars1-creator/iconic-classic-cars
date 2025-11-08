6eba8a1bd94926e3d9cd944a7df8d75870968162
iconicclassiccars1-creator <iconicclassiccars1@gmail.com>
2025-11-08 09:04:41 +0100

    Fix syntax projecten array en voeg Ford Transit toe

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,13 @@ export default function Page() {
 
   ];
   const projecten = [
-    {title:'Volvo Amazon – dorpels & vloer', img:'/img/ph1.svg', desc:'Deelrestauratie met OEM-plaatwerk, fotolog & rapport. Doorlooptijd: 6 weken.'},
-    {title:'Volvo 940 – airco & remmen',     img:'/img/ph2.svg', desc:'Compleet onderhoud + airco-reparatie, testrit & opleverchecklist.'},
-    {title:'Redblock – carb & ontsteking',   img:'/img/ph3.svg', desc:'SU-revisie, ontstekingscurve optimalisatie en afsteltijd op de bank/weg.'}
-    {title:'Ford Transit MK1 – project,      img:'/projecten/ford-transit-mk1/voorzijde.jpg', desc: 'Volledige restauratie & technische update van een Ford Transit MK1.', href: '/projecten/ford-transit-mk1',
-},
+    {title: 'Volvo Amazon – dorpels & vloer', img: '/img/ph1.svg', desc:'Deelrestauratie met OEM-plaatwerk, fotolog & rapport. Doorlooptijd: 6 weken.',},
+    {title: 'Volvo 940 – airco & remmen',     img: '/img/ph2.svg', desc:'Compleet onderhoud + airco-reparatie, testrit & opleverchecklist.',},
+    {title: 'Redblock – carb & ontsteking',   img: '/img/ph3.svg', desc:'SU-revisie, ontstekingscurve optimalisatie en afsteltijd op de bank/weg.',},
+    {title: 'Ford Transit MK1 – project',     img: '/projecten/ford-transit-mk1/voorzijde.jpg', desc:'Volledige restauratie & technische update van een Ford Transit MK1.',
+    href: '/projecten/ford-transit-mk1',},
+  ];
+
 
   ];
   return (
