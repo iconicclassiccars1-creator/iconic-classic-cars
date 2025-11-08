8fd4fa614ca541e4f029a87d141e77ed47976565
iconicclassiccars1-creator <iconicclassiccars1@gmail.com>
2025-11-08 06:47:00 +0100

    Ford Transit MK1 project toegevoegd

---
+++ app/projecten/ford-transit-mk1/page.tsx
@@ -0,0 +1,23 @@
+﻿const projecten = [
+  {
+    title: 'Volvo Amazon – dorpels & vloer',
+    img: '/img/ph1.svg',
+    desc: 'Deelrestauratie met OEM-plaatwerk, fotolog & rapport. Doorlooptijd: 6 weken.',
+  },
+  {
+    title: 'Volvo 940 – airco & remmen',
+    img: '/img/ph2.svg',
+    desc: 'Compleet onderhoud + airco-reparatie, testrit & opleverchecklist.',
+  },
+  {
+    title: 'Redblock – carb & ontsteking',
+    img: '/img/ph3.svg',
+    desc: 'SU-revisie, ontstekingscurve optimalisatie en afsteltijd op de bank/weg.',
+  },
+  {
+    title: 'Ford Transit MK1 – project',
+    img: '/img/ph4.svg', // vervang dit door je eigen foto
+    desc: 'Volledige restauratie & technische update van een Ford Transit MK1. Carrosserie, onderstel en aandrijflijn in ere hersteld.',
+    href: '/projecten/ford-transit-mk1',
+  },
+];
